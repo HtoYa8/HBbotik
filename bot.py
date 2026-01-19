f32ecb8cf6c34193b98df634cc427dfc366ce7a9
HtoYa <A_Kirill_V@mail.ru>
2026-01-19 19:49:36 +0300

    on_ready ивент

--- bot.py
+++ bot.py
@@ -10,6 +10,10 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+@bot.event()
+async def on_ready():
+    print(f"Бот {bot.user} запущен")
+
 @bot.command(name='list')
 async def list(ctx, arg):
     await ctx.send(arg)
